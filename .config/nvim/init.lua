9389a887733769565c99dc029289ee1d5181c8df
Lars Petter Hauge <larsenhauge@gmail.com>
2023-12-02 08:45:36 +0100

    Relative numbers

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -12,6 +12,7 @@ vim.opt.clipboard="unnamedplus" -- access clipboard outside of vim
 vim.opt.mouse=n
 vim.opt.laststatus=2
 vim.opt.number=true             -- add line number
+vim.opt.relativenumber=true     -- and make them relative
 vim.opt.syntax="on"
 vim.opt.scrolloff=20            -- keep n lines above/below cursor (pad lines around cursor)
 
