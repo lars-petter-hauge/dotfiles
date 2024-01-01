b5959f01129ef7e614410fcc71f35bcbb1b8a88d
Lars Petter Hauge <larsenhauge@gmail.com>
2024-01-01 19:59:37 +0100

    Remove unused on_attach

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -283,9 +283,7 @@ mason_lspconfig.setup_handlers {
   function(server_name)
     require('lspconfig')[server_name].setup {
       capabilities = capabilities,
-      on_attach = on_attach,
       settings = servers[server_name],
-      filetypes = (servers[server_name] or {}).filetypes,
     }
   end,
 }
