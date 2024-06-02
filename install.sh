4b1195e7d190ed484ab9afebaf1e92072a7a5d02
Lars Petter Hauge <larsenhauge@gmail.com>
2024-06-02 18:55:42 +0200

    Changing to using LazyVim instead of selfmade config

--- install.sh
+++ install.sh
@@ -3,14 +3,17 @@
 olddir=${HOME}/dotfiles_old
 mkdir -p $olddir
 
-files=".alias .zshrc .p10k.zsh .tmux.conf .vimrc .config/nvim"
-
+files=".alias .zshrc .p10k.zsh .tmux.conf .vimrc"
 
 for file in $files; do
-    if [ -f ${HOME}/$file ]; then
-        echo "Moving existing $file -> $olddir/$file"
-        mv ${HOME}/$file $olddir/
-    fi
-
-    ln -s  $(pwd)/$file ${HOME}/$file
+	if [ -f ${HOME}/$file ]; then
+    if [ -L ${HOME}/$file ]; then
+		  echo "Found existing symlink for $file, unlinking"
+		  unlink ${HOME}/$file
+    else
+      echo "Moving existing $file -> $olddir/$file"
+      mv ${HOME}/$file $olddir/
+	fi
+  echo "Creating symlink for ${HOME}/$file $(pwd)/$file " 
+	ln -s $(pwd)/$file ${HOME}/$file
 done
