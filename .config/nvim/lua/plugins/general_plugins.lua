6c2e33658e3986f5bb038b82435bbf801b2575b7
Lars Petter Hauge <larsenhauge@gmail.com>
2024-06-03 21:25:33 +0200

    Include fugitive and vim-surround in lazyvim setup

---
+++ .config/nvim/lua/plugins/general_plugins.lua
@@ -0,0 +1,10 @@
+return {
+	{
+		"tpope/vim-fugitive",
+		keys = {
+			{ "<leader>gB", "<cmd>Git blame<cr>", desc = "Git blame in gutter" },
+			{ "<leader>gd", "<cmd>Gvdiffsplit<cr>", desc = "Diff split vertical buffers" },
+		},
+	},
+	{ "tpope/vim-surround" },
+}
