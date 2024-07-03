9f3de455b578abecadfad57be1fb8980d610fdaf
Lars Petter Hauge <larsenhauge@gmail.com>
2024-07-03 21:45:09 +0200

    Disabled several unused plugins

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,3 +1,9 @@
 return {
 	{ "folke/noice.nvim", enabled = false },
+	{ "nvim-pack/nvim-spectre", enabled = false }, -- Search replace in multiple files. If needed outside of lsp, add it
+	{ "folke/todo-comments.nvim", enabled = false }, -- Dont really use todo in code..
+	{ "echasnovski/mini.pairs", enabled = false }, -- Adding espacing brackets, useful plugin, but also slightly annoying
+	{ "rafamadriz/friendly-snippets", enabled = false }, -- Don't really use snippets
+	{ "garymjr/nvim-snippets", enabled = false }, -- Don't really use snippets
+	{ "echasnovski/mini.ai", enabled = false }, -- More exhaustive text object identification. Perhaps good, but need to learn default bette More exhaustive text object identification. Perhaps good, but need to learn default betterr
 }
