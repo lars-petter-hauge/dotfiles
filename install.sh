543c6aee5a9538c3b13115cff6f977c9bd09e6e1
Lars Petter Hauge <larsenhauge@gmail.com>
2024-09-17 20:30:44 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -3,17 +3,29 @@
 olddir=${HOME}/dotfiles_old
 mkdir -p $olddir
 
-files=".alias .zshrc .p10k.zsh .tmux.conf .vimrc"
+files=("
+  .alias
+  .zshrc
+  .p10k.zsh
+  .tmux.conf
+  .vimrc
+  .config/nvim/lua/config/keymaps.lua
+  .config/nvim/lua/plugins/disabled.lua
+  .config/nvim/lua/plugins/flash.lua
+  .config/nvim/lua/plugins/general_plugins.lua
+")
 
 for file in $files; do
-	if [ -f ${HOME}/$file ]; then
-    if [ -L ${HOME}/$file ]; then
-		  echo "Found existing symlink for $file, unlinking"
-		  unlink ${HOME}/$file
+  target=${HOME}/$file
+  if [ -f $target ]; then
+    if [ -L $target ]; then
+      echo "Found existing symlink for $file, unlinking"
+      unlink $target
     else
       echo "Moving existing $file -> $olddir/$file"
-      mv ${HOME}/$file $olddir/
-	fi
-  echo "Creating symlink for ${HOME}/$file $(pwd)/$file " 
-	ln -s $(pwd)/$file ${HOME}/$file
+      mv $target $olddir/
+    fi
+  fi
+  echo "Creating symlink for $target $(pwd)/$file "
+  ln -s $(pwd)/$file $target
 done
