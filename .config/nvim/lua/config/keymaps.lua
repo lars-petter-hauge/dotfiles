e5f5cbb87665577bdc06de222dcaeaed276ee374
Lars Petter Hauge <larsenhauge@gmail.com>
2024-09-17 16:02:20 +0200

    Use same splits as tmux

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,7 +1,14 @@
 local map = vim.keymap.set
+local delete = vim.keymap.del
 
 -- s is vim built-in, mapped to change single char under cursor
 -- Identical behaviour is achieved with e.g. cl. We want to use
 -- surround with the least amount of overhead and therefore remap
 map("n", "s", "ys", { desc = "Surround", remap = true })
 map("v", "s", "S", { desc = "Surround", remap = true })
+
+-- Use same split as in tmux
+map("n", "<leader>wh", "<C-W>s", { desc = "Split Window Below", remap = true })
+map("n", "<leader>wv", "<C-W>v", { desc = "Split Window Right", remap = true })
+delete("n", "<leader>w|")
+delete("n", "<leader>w-")
