f5ab057d5c97c18b111acb8bb9536fdd2f614de6
Lars Petter Hauge <larsenhauge@gmail.com>
2025-04-24 10:56:43 +0200

    Add keymapping for running neotest with dap

--- .config/nvim/lua/plugins/neotest.lua
+++ .config/nvim/lua/plugins/neotest.lua
@@ -5,6 +5,15 @@ return {
       "nvim-neotest/neotest-python",
       "nvim-treesitter/nvim-treesitter",
     },
+    keys = {
+      {
+        "<leader>td",
+        function()
+          require("neotest").run.run({ strategy = "dap" })
+        end,
+        desc = "Run Nearest (neotest debug)",
+      },
+    },
     opts = {
       adapters = {
         ["neotest-python"] = {
