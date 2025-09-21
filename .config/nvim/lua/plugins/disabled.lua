0ab83f9a507adb328052d802dececa42b49fe9fa
Lars Petter Hauge <larsenhauge@gmail.com>
2025-09-21 11:33:02 +0200

    Update origin of mini plugins

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -2,9 +2,9 @@ return {
 	{ "folke/noice.nvim", enabled = false },
 	{ "akinsho/bufferline.nvim", enabled = false }, -- Tab view - don't really use tabs, requires mouse
 	{ "folke/todo-comments.nvim", enabled = false }, -- Dont really use todo in code..
-	{ "echasnovski/mini.pairs", enabled = false }, -- Adding espacing brackets, useful plugin, but also slightly annoying
+	{ "nvim-mini/mini.pairs", enabled = false }, -- Adding espacing brackets, useful plugin, but also slightly annoying
 	{ "rafamadriz/friendly-snippets", enabled = false }, -- Don't really use snippets
 	{ "garymjr/nvim-snippets", enabled = false }, -- Don't really use snippets
-	{ "echasnovski/mini.ai", enabled = false }, -- More exhaustive text object identification. Perhaps good, but need to learn default bette More exhaustive text object identification. Perhaps good, but need to learn default betterr
+	{ "nvim-mini/mini.ai", enabled = false }, -- More exhaustive text object identification. Perhaps good, but need to learn default better
 	{ "folke/persistence.nvim", enabled = false }, -- session management. Currently not used much.. perhaps marks is the only thing I would like to store. I should rather improve on not closing nvim all the time
 }
