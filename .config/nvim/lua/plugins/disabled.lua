21b2f3d5606792ebba17a03436946a7f1843d45f
Lars Petter Hauge <larsenhauge@gmail.com>
2026-03-04 09:19:21 +0100

    Disable default copilot plugin for now

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -5,6 +5,7 @@ return {
 	{ "garymjr/nvim-snippets", enabled = false }, -- Don't really use snippets
 	{ "folke/persistence.nvim", enabled = false }, -- session management. Currently not used much.. perhaps marks is the only thing I would like to store. I should rather improve on not closing nvim all the time
 	{ "rcarriga/nvim-dap-ui", enabled = false }, -- Default dap extra ui. Must be disabled in order to not conflict with nvim-dap-view
+	{ "zbirenbaum/copilot.nvim", enabled = false }, -- already using copilot.vim
 	{
 		"mfussenegger/nvim-dap",
 		dependencies = {
